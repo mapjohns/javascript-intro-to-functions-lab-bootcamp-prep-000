b387c2cf6c2b3bb168ce25338ad7d68ba0def115
Matthew Johnson <mattj20@gmail.com>
2021-04-08 00:42:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,5 @@ function sayHiToGrandma(string) {
   else if (string === "I love you, Grandma.") {
     console.log("I love you, too.")
   }
-}
-
-console.log(sayHiToGrandma('I love you, Grandma')
+}onsole.log(sayHiToGrandma('I love you, Grandma')
 
