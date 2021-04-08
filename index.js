0383a99d0906af1601745fa2896cee21dd96473e
Matthew Johnson <mattj20@gmail.com>
2021-04-08 00:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    console.log("I can't hear you!")
-  } else if (string === string.toUpperCase()) {
-    console.log('YES INDEED!')
-  }
-  else if (string === "I love you, Grandma.") {
-    console.log("I love you, too.")
-  }
-}onsole.log(sayHiToGrandma('I love you, Grandma')
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!"
+    }
+    else if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+    }
+    else if (string === "I love you, Grandma.") {
+        return "I love you, too."
+    }
+    else {
+        return "idk my BFF Jill"
+    }
+}
 
